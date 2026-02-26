d990b6d9b6b24efb7362ea9aa076400af624de35
EmnaNefzi <152291766+EmnaNefzi@users.noreply.github.com>
2026-02-26 03:31:27 +0100

    Create sa_players_men table in the database

---
+++ Database/SA/create_sa_players_men.sql
@@ -0,0 +1,9 @@
+CREATE TABLE sa.sa_players_men (
+    nome        VARCHAR(255),
+    country     VARCHAR(50),
+    points      VARCHAR(50),
+    position    VARCHAR(50),
+    move        VARCHAR(50),
+    source_file VARCHAR(255) NOT NULL,
+    page_no     INTEGER
+);
