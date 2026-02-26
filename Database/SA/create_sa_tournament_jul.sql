f20082cafd19578dfe7fde72410db4f678fa08c2
EmnaNefzi <152291766+EmnaNefzi@users.noreply.github.com>
2026-02-26 03:16:45 +0100

    Create sa_tournament_jul table in database

--- Database/SA/create_sa_tournament_jul.sql
+++ Database/SA/create_sa_tournament_jul.sql
@@ -1,1 +1,17 @@
-
+CREATE TABLE sa.sa_tournament_jul (
+    joueur1_nom VARCHAR(100),
+    joueur1_prenom VARCHAR(100),
+    joueur1_nationalite VARCHAR(50),
+    joueur2_nom VARCHAR(100),
+    joueur2_prenom VARCHAR(100),
+    joueur2_nationalite VARCHAR(50),
+    seed INTEGER,
+    round VARCHAR(50),
+    score VARCHAR(50),
+    resultat VARCHAR(20),
+    date_tournoi DATE,
+    nom_tournoi VARCHAR(150),
+    lieu_tournoi VARCHAR(150),
+    prize_money NUMERIC(12,2),
+    points INTEGER
+  );
