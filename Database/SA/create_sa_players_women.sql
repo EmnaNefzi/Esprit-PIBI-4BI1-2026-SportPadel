b6de8190caa24d6d62d2ccfef1f7b35927d8fd70
EmnaNefzi <152291766+EmnaNefzi@users.noreply.github.com>
2026-02-26 03:32:40 +0100

    Create create_sa_players_women.sql

---
+++ Database/SA/create_sa_players_women.sql
@@ -0,0 +1,9 @@
+CREATE TABLE sa.sa_players_women (
+    id BIGSERIAL PRIMARY KEY,
+    name          VARCHAR(200),
+    country       VARCHAR(100),
+    points        VARCHAR(50),
+    position      INTEGER,
+    move          VARCHAR(50),
+    page_no       INTEGER
+  );
